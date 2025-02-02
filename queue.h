1ff843bece37de6cd30e1547a2d4363e819d6ac7
lays <vaseto.stoianov.94@gmail.com>
2025-02-02 16:43:35 +0200

    Fixes

--- queue.h
+++ queue.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "stdlib.h"
+#include <string.h>
 int MAX_SIZE = 25;
 
 typedef struct QNode
@@ -12,19 +13,55 @@ typedef struct QNode
 
 typedef struct Queue {
    int size;
-   QNode head;
-   QNode tail;
+   QNode *head;
+   QNode *tail;
 } Queue;
 
 
-int pop(QNode *head, QNode  toPut){
-    if(head->value == NULL){
-       head->next = NULL;
-       head->previous = NULL;
+int push(QNode *head, QNode  toPut,Queue * queue){
+    if(head == NULL){
+        head = malloc(sizeof(QNode));
+       head->next = malloc(sizeof(sizeof(QNode)));
+       head->previous = malloc(sizeof(sizeof(QNode)));
        head->value = malloc(sizeof((char*)toPut.value));
        memcpy((char*)head->value,(char*)toPut.value,sizeof((char*)toPut.value));
-    }
+       queue->tail = head->next;
+      queue->head = head;
     return 1;
-  
+    }
+    if(head->next->value ==NULL){
+        head->next->next = malloc(sizeof(sizeof(QNode)));
+        head->next->previous = head;
+       head->next->value = malloc(sizeof((char*)toPut.value));
+       memcpy((char*)head->next->value,(char*)toPut.value,sizeof((char*)toPut.value));
+       queue->tail = head->next;
+       return 1;
+    }else {
+        push(head->next,toPut,queue);
+    }
+
+    return 0;  
+}
+
+QNode* pop(Queue* queue){
+   if(queue->head == NULL){
+    return NULL;
+   }
+   QNode* res = queue->head;
+   queue->head = queue->head->next;
+   queue->head->previous = NULL;
+return res;
+}
+
+
+void freeQueue(Queue * queue){
+    QNode * current = queue->head;
+    while(current != NULL){
+        QNode * next = current->next;
+        free(current->value);
+        free(current);
+        current = next;
+    }
+    free(queue);
 }
 
